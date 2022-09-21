a8065fa1ee05f11ff661472731c6d473e664806b
Yohan You <yohan.you@ug.bilkent.edu.tr>
2022-09-21 15:28:57 +0300

    UPDATED
    including array generation + basics of UI

--- IntegerArray.java
+++ IntegerArray.java
@@ -10,29 +10,32 @@ public class IntegerArray {
     // display the menu options, get the user's choice, call the methods
     // until the user decides to exit, keep displaying it
     public static void main(String[] args) {
+        int programContinuation = 1;        
 
+        //Elkhan's implementation of random number generator + array generation
         int min = 0;
         int max = 100;
         int[] nums = new int[10];
 
-        
+        //for loop to change each empty element with a random number between [1,100]
         for (int i = 0; i < nums.length; i++) {
             nums[i] = (int) (Math.random() * (max - min + 1) + min);
         }
 
+        //prints out the array list
         System.out.println(Arrays.toString(nums));
 
-        int programContinuation = 1;        
-
+        //scanner for user input
         Scanner in = new Scanner(System.in);
 
+        //yohan you's implementation of some sort of user interface
         while (programContinuation > 0)
         {
             System.out.println("Please choose what you would like to do:");
             System.out.println("1. Find the minimum and maximum of the array");
             System.out.println("2. Find the average of the array");
             System.out.println("3. find the sum of the elements with odd and even numbered indexes");
-            System.out.println("9. Exit the program.");
+            System.out.println("0. Exit the program.");
     
             int userInput = in.nextInt();
 
@@ -51,7 +54,7 @@ public class IntegerArray {
     
                 }
     
-            if (userInput == 4)
+            if (userInput == 0)
                 {
                     programContinuation = 0;
                     in.close();
