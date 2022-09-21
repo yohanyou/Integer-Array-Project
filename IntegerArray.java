e203fbf363a800acad43226fb3d0ae3010b3d912
Yohan You <yohan.you@ug.bilkent.edu.tr>
2022-09-21 15:04:52 +0300

    main method

--- IntegerArray.java
+++ IntegerArray.java
@@ -7,9 +7,11 @@ public class IntegerArray {
 
     //display the menu options, get the user's choice, call the methods
     //until the user decides to exit, keep displaying it
-    System.out.println("Please choose what you would like to do: ");
-    System.out.print("");
+    public static void main(String[] args){
+        System.out.println("Please choose what you would like to do:");
+        System.out.println("");
 
+    }
 
     //find the minimum and maximum of the array, implemented as different methods
 
