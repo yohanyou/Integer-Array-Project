19ac90b72ad8b220cba10316ab526db7cff11bb2
Yohan You <yohan.you@ug.bilkent.edu.tr>
2022-09-21 14:52:02 +0300

    Finished forming the skeleton
    please pull this and work on each section :)

--- IntegerArray.java
+++ IntegerArray.java
@@ -1,1 +1,25 @@
+//this class is used to solve all sorts of IntegerArray problems
+public class IntegerArray {
 
+    //create an int array that contains random integers in the range [0,100]
+
+
+
+    //display the menu options, get the user's choice, call the methods
+    //until the user decides to exit, keep displaying it
+
+
+
+    //find the minimum and maximum of the array, implemented as different methods
+
+
+
+    //find the average of the array, then display the differences of each array to the average
+
+
+
+    //find the sum of the odd and even numbered indexes 
+
+
+
+}
